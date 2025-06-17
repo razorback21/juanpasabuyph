4e38a34e96dc68aa32c3c25783630d6d7df819aa
Donz <donnygapulao@gmail.com>
2025-06-17 23:28:15 +0800

    prop fixes

--- resources/js/Components/Pagination.jsx
+++ resources/js/Components/Pagination.jsx
@@ -1,6 +1,6 @@
 import { Link } from '@inertiajs/react';
 
-export default (links) => {
+export default ({links}) => {
     return <>
         {Object.entries(links).map(([key, value]) => (
             <Link href={value.url??'#'} key={key}>
