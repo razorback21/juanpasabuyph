cf2236ff97b26d8a6fbeab91ffe6a536ad7b8baa
Donz <donnygapulao@gmail.com>
2025-06-18 11:48:08 +0800

    added style to pagination

--- resources/js/Components/Pagination.jsx
+++ resources/js/Components/Pagination.jsx
@@ -1,11 +1,17 @@
 import { Link } from '@inertiajs/react';
 
 export default ({links}) => {
-    return <>
+    return <div className="flex items-center justify-center space-x-2 mt-4">
         {Object.entries(links).map(([key, value]) => (
-            <Link href={value.url??'#'} key={key}>
-                {value.label}
+            <Link 
+                href={value.url??'#'} 
+                key={key}
+                className={`px-4 py-2 rounded-md ${value.active 
+                    ? 'bg-gray-600 text-white font-medium' 
+                    : 'bg-white text-gray-700 hover:bg-gray-100 border'}`}
+            >
+                {value.label.replace('&laquo;', '«').replace('&raquo;', '»')}
             </Link>
         ))}
-    </>
+    </div>
 }
