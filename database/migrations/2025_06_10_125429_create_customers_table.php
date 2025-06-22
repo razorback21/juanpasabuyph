6020611f3edf026db5a8fed30967f4f07b600610
Donz <donnygapulao@gmail.com>
2025-06-22 11:22:25 +0800

    customers migration

--- database/migrations/2025_06_10_125429_create_customers_table.php
+++ database/migrations/2025_06_10_125429_create_customers_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
             $table->timestamps();
         });
     }
