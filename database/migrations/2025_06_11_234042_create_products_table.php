b66033fdd270418e4aac16322944284db4a3a988
Donz <donnygapulao@gmail.com>
2025-06-22 11:22:43 +0800

    products table migration

--- database/migrations/2025_06_11_234042_create_products_table.php
+++ database/migrations/2025_06_11_234042_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\ProductCategory;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,8 +17,9 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->decimal('price', 10, 2);
-            $table->boolean('is_featured')->default(false);
+            $table->foreignIdFor(ProductCategory::class);
             $table->string('featured_image')->nullable();
+            $table->boolean('is_featured')->default(false);
             $table->string('socialmedia_image')->nullable();
             $table->boolean('disabled')->default(false);
             $table->timestamps();
