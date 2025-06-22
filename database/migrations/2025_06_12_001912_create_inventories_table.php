51ab108488ae85c5f2d8aa9f0edf5f09697b63b3
Donz <donnygapulao@gmail.com>
2025-06-22 12:52:50 +0800

    added foreignIdFor fro Order

--- database/migrations/2025_06_12_001912_create_inventories_table.php
+++ database/migrations/2025_06_12_001912_create_inventories_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Order;
 use App\Models\Product;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -15,10 +16,11 @@ return new class extends Migration
         Schema::create('inventories', function (Blueprint $table) {
             $table->id();
             $table->integer('quantity');
-            $table->foreignIdFor(Product::class)->constrained()->onDelete('cascade');
+            $table->foreignIdFor(Product::class)->onDelete('cascade');
             $table->string('uom');
             $table->string('movement_type')->default('inbound');
-            $table->text('notes')->nullable(); 
+            $table->text('notes')->nullable();
+            $table->foreignIdFor(Order::class)->nullable()->onDelete('cascade');
             $table->timestamps();
         });
     }
