f6a59be295bcee5bb0bad8ba85e4936b41e8ae90
Donz <donnygapulao@gmail.com>
2025-06-22 11:22:56 +0800

    inventories table migration

--- database/migrations/2025_06_12_001912_create_inventories_table.php
+++ database/migrations/2025_06_12_001912_create_inventories_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,10 +14,11 @@ return new class extends Migration
     {
         Schema::create('inventories', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->integer('quantity');
-            $table->enum('movement_type', ['in', 'out']);
-            $table->text('notes')->nullable();
+            $table->foreignIdFor(Product::class)->constrained()->onDelete('cascade');
+            $table->string('uom');
+            $table->string('movement_type')->default('inbound');
+            $table->text('notes')->nullable(); 
             $table->timestamps();
         });
     }
