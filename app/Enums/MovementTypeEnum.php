85fb185612b089c96db1331fa2962c7af6f55d83
Donz <donnygapulao@gmail.com>
2025-06-22 12:11:37 +0800

    additonal status for movement type

--- app/Enums/MovementTypeEnum.php
+++ app/Enums/MovementTypeEnum.php
@@ -7,4 +7,6 @@ enum MovementTypeEnum : string
     case RESERVED = 'reserved';
     case INBOUND = 'inbound';
     case OUTBOUND = 'outbound';
+    case RETURNED = 'returned';
+    case ADJUSTMENT = 'adjustment';
 }
