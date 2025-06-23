3624b09c8882603866cd3cc0f3721731a9e65c82
Donz <donnygapulao@gmail.com>
2025-06-23 14:54:55 +0800

    feat(dashboard): add data.json and comment out SectionCards
    Add sample project data in JSON format for dashboard visualization.
    Temporarily disable SectionCards component for testing ChartAreaInteractive.

--- resources/js/Pages/dashboard.jsx
+++ resources/js/Pages/dashboard.jsx
@@ -5,7 +5,8 @@ import { SectionCards } from "@/components/section-cards"
 import { SiteHeader } from "@/components/site-header"
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 
-import data from "./data.json"
+import data from "./data.json";
+
 
 export default function Page() {
   return (
@@ -16,7 +17,7 @@ export default function Page() {
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <SectionCards />
+              {/* <SectionCards /> */}
               <div className="px-4 lg:px-6">
                 <ChartAreaInteractive />
               </div>
