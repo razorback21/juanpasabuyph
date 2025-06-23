9565173cd8023dc647d2bbada2cde34af8f249b8
Donz <donnygapulao@gmail.com>
2025-06-23 16:36:39 +0800

    comment out extar menus

--- resources/js/Components/nav-main.jsx
+++ resources/js/Components/nav-main.jsx
@@ -35,7 +35,7 @@ export function NavMain({
         <SidebarMenu>
           {items.map((item) => (
             <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton tooltip={item.title}>
+              <SidebarMenuButton tooltip={item.title} isActive={item.isActive}>
                 {item.icon && <item.icon />}
                 <span>{item.title}</span>
               </SidebarMenuButton>
