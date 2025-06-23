30e1bdc858515d20b42a7bc1686dd5ef5d5235c6
Donz <donnygapulao@gmail.com>
2025-06-23 15:44:01 +0800

    products table initail code

--- resources/js/Pages/Dashboard/Index.jsx
+++ resources/js/Pages/Dashboard/Index.jsx
@@ -1,33 +1,45 @@
+// import { DataTable } from "@/components/data-table"
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
-import Pagination from "@/Components/Pagination";
-import ProductsTable from "@/Pages/Dashboard/ProductsTable";
+import { ProductsTable } from "./ProductsTable";
 
-export default function({ products, categories, active_category }) {
+import data from "../data.json";
+
+export default function() {
     return (
-        <AuthenticatedLayout
-            header={
-                <h2 className="text-xl font-semibold leading-tight text-gray-800">
-                    Dashboard
-                </h2>
-            }
-        >
+        <AuthenticatedLayout>
             <Head title="Dashboard" />
-
-            <div className="py-12">
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 bg-white">
-                    <div className="overflow-hidden px-6 py-12 bg-white shadow-sm sm:rounded-lg">
-                        <ProductsTable
-                            products={products}
-                            categories={categories}
-                            active_category={active_category}
-                        />
-                    </div>
-                </div>
-                <div className="text-gray-900 text-center">
-                    <Pagination links={products.links} />
-                </div>
-            </div>
-        </AuthenticatedLayout>
-    );
+            <ProductsTable data={data} />
+        </AuthenticatedLayout>    
+    )
 }
+    
+
+// export default function({ products, categories, active_category }) {
+//     return (
+//         <AuthenticatedLayout
+//             header={
+//                 <h2 className="text-xl font-semibold leading-tight text-gray-800">
+//                     Dashboard
+//                 </h2>
+//             }
+//         >
+//             <Head title="Dashboard" />
+
+//             <div className="py-12">
+//                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 bg-white">
+//                     <div className="overflow-hidden px-6 py-12 bg-white shadow-sm sm:rounded-lg">
+//                         <ProductsTable
+//                             products={products}
+//                             categories={categories}
+//                             active_category={active_category}
+//                         />
+//                     </div>
+//                 </div>
+//                 <div className="text-gray-900 text-center">
+//                     <Pagination links={products.links} />
+//                 </div>
+//             </div>
+//         </AuthenticatedLayout>
+//     );
+// }
