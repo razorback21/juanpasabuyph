5f7f0d544d0783baa33f542547a909f69d2f8c7d
Donz <donnygapulao@gmail.com>
2025-06-23 16:46:08 +0800

    Dashboard index shadcn

--- resources/js/Pages/Dashboard/Index.jsx
+++ resources/js/Pages/Dashboard/Index.jsx
@@ -1,45 +1,19 @@
-// import { DataTable } from "@/components/data-table"
+//import { DataTable } from "@/components/data-table"
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
 import { ProductsTable } from "./ProductsTable";
 
-import data from "../data.json";
-
-export default function() {
+export default function ({ products, categories, active_category }) {
     return (
         <AuthenticatedLayout>
             <Head title="Dashboard" />
-            <ProductsTable data={data} />
-        </AuthenticatedLayout>    
-    )
+            <div className="flex flex-col px-4 lg:px-6">
+                <ProductsTable
+                    products={products}
+                    categories={categories}
+                    active_category={active_category}
+                />
+            </div>
+        </AuthenticatedLayout>
+    );
 }
-    
-
-// export default function({ products, categories, active_category }) {
-//     return (
-//         <AuthenticatedLayout
-//             header={
-//                 <h2 className="text-xl font-semibold leading-tight text-gray-800">
-//                     Dashboard
-//                 </h2>
-//             }
-//         >
-//             <Head title="Dashboard" />
-
-//             <div className="py-12">
-//                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 bg-white">
-//                     <div className="overflow-hidden px-6 py-12 bg-white shadow-sm sm:rounded-lg">
-//                         <ProductsTable
-//                             products={products}
-//                             categories={categories}
-//                             active_category={active_category}
-//                         />
-//                     </div>
-//                 </div>
-//                 <div className="text-gray-900 text-center">
-//                     <Pagination links={products.links} />
-//                 </div>
-//             </div>
-//         </AuthenticatedLayout>
-//     );
-// }
