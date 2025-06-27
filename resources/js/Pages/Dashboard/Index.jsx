e287cd1aad7e9a6c2ddd5c72bed0b9d4f14ea307
Donz <donnygapulao@gmail.com>
2025-06-27 11:07:34 +0800

    pass th ewgole dashboard view variables

--- resources/js/Pages/Dashboard/Index.jsx
+++ resources/js/Pages/Dashboard/Index.jsx
@@ -1,17 +1,15 @@
 //import { DataTable } from "@/components/data-table"
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
-import { ProductsTable } from "./ProductsTable";
+import { ProductsTable } from "./ProductsTable.jsx.TanStack";
 
-export default function ({ products, categories, active_category }) {
+export default function (props) {
     return (
         <AuthenticatedLayout>
             <Head title="Dashboard" />
             <div className="flex flex-col px-4 lg:px-6">
                 <ProductsTable
-                    products={products}
-                    categories={categories}
-                    active_category={active_category}
+                   {...props}
                 />
             </div>
         </AuthenticatedLayout>
