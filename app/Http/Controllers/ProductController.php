dd529523d88f58042b4031091aeec19e4a3e3ce1
Donz <donnygapulao@gmail.com>
2025-06-27 11:09:29 +0800

    update validation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -92,7 +92,7 @@ class ProductController extends Controller
             'name' => 'required|string|max:255',
             'description' => 'nullable|string',
             'price' => 'required|numeric|min:0',
-            'product_category_id' => 'required',
+            'product_category_id' => 'required|exists:product_categories,id',
         ]);
 
         $product->update($validated);
