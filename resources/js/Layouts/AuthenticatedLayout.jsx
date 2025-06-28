4aa7de94161597db9f07e99a7c9bf216795c551f
Donz <donnygapulao@gmail.com>
2025-06-28 13:02:38 +0800

    added products

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -2,13 +2,13 @@ import { AppSidebar } from "@/components/app-sidebar"
 import { SiteHeader } from "@/components/site-header"
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 
-export default function AuthenticatedLayout({ header, children }) {
+export default function AuthenticatedLayout({ header = 'Dashboard', children }) {
 
     return (
         <SidebarProvider>
             <AppSidebar variant="inset" />
             <SidebarInset>
-                <SiteHeader title="Products" />
+                <SiteHeader title={header} />
                 <div className="flex flex-1 flex-col">
                     <div className="@container/main flex flex-1 flex-col gap-2">
                         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
