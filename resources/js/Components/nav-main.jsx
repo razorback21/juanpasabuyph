dd0d15a52f7456e3d00c8cdbf637d8fb56b7392a
Donz <donnygapulao@gmail.com>
2025-06-28 12:56:50 +0800

    active logic

--- resources/js/Components/nav-main.jsx
+++ resources/js/Components/nav-main.jsx
@@ -1,48 +1,53 @@
-import { MailIcon, PlusCircleIcon } from "lucide-react";
+import LinkWithChildren from "./LinkWithChildren";
+import { usePage } from "@inertiajs/react";
 
-import { Button } from "@/components/ui/button"
 import {
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "@/components/ui/sidebar"
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "@/components/ui/sidebar";
 
-export function NavMain({
-  items
-}) {
-  return (
-    <SidebarGroup>
-      <SidebarGroupContent className="flex flex-col gap-2">
-        <SidebarMenu>
-          <SidebarMenuItem className="flex items-center gap-2">
-            {/* <SidebarMenuButton
+export function NavMain({ items }) {
+    const ziggy = usePage().props.ziggy;
+    console.log(ziggy.location.includes('dashboard'));
+    return (
+        <SidebarGroup>
+            <SidebarGroupContent className="flex flex-col gap-2">
+                <SidebarMenu>
+                    <SidebarMenuItem className="flex items-center gap-2">
+                        {/* <SidebarMenuButton
               tooltip="Quick Create"
               className="min-w-8 bg-primary text-primary-foreground duration-200 ease-linear hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground">
               <PlusCircleIcon />
               <span>Quick Create</span>
             </SidebarMenuButton> */}
-            {/* <Button
+                        {/* <Button
               size="icon"
               className="h-9 w-9 shrink-0 group-data-[collapsible=icon]:opacity-0"
               variant="outline">
               <MailIcon />
               <span className="sr-only">Inbox</span>
             </Button> */}
-          </SidebarMenuItem>
-        </SidebarMenu>
-        <SidebarMenu>
-          {items.map((item) => (
-            <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton tooltip={item.title} isActive={item.isActive}>
-                {item.icon && <item.icon />}
-                <span>{item.title}</span>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          ))}
-        </SidebarMenu>
-      </SidebarGroupContent>
-    </SidebarGroup>
-  );
+                    </SidebarMenuItem>
+                </SidebarMenu>
+                <SidebarMenu>
+                    {items.map((item) => (
+                        <SidebarMenuItem key={item.title}>
+                            <LinkWithChildren href={route(item.route)}>
+                                <SidebarMenuButton
+                                    tooltip={item.title}
+                                    isActive={ziggy.location.includes(item.activeKey)}
+                                >
+                                    {item.icon && <item.icon />}
+                                    <span>{item.title}</span>
+                                </SidebarMenuButton>
+                            </LinkWithChildren>
+                        </SidebarMenuItem>
+                    ))}
+                </SidebarMenu>
+            </SidebarGroupContent>
+        </SidebarGroup>
+    );
 }
