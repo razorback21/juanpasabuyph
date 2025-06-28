b3a95e4fe9d18f892d0f8544fd6aa433333b7fc7
Donz <donnygapulao@gmail.com>
2025-06-28 13:13:30 +0800

    renamed render template

--- app/Http/Controllers/ProductCategoryController.php
+++ app/Http/Controllers/ProductCategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ProductCategoryController extends Controller
 {
@@ -11,7 +12,7 @@ class ProductCategoryController extends Controller
      */
     public function index()
     {
-        //
+        return Inertia::render('ProductCategory/Index');
     }
 
     /**
