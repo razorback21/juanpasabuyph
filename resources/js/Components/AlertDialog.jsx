0ec6cd06fcca236de53a091b5ca2c13996056172
Donz <donnygapulao@gmail.com>
2025-07-03 18:25:53 +0800

    fixed alert dialog

---
+++ resources/js/Components/AlertDialog.jsx
@@ -0,0 +1,60 @@
+import { forwardRef, useRef, useImperativeHandle, useState } from "react";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+
+const ConfirmAlert = forwardRef(({ children, buttonName = 'Continue' }, ref) => {
+    const [open, setOpen] = useState(false);
+    const [dialogProps, setDialogProps] = useState({
+        title:'',
+        description:'',
+        onContinue: () => {},
+    });
+    const { title, description, onContinue } = dialogProps;
+
+    useImperativeHandle(ref, () => ({
+        open() {
+            setOpen(true)
+        },
+        close() {
+            setOpen(false);
+        },
+        setDialogProps(props) {
+            setDialogProps(props);
+        }
+    }));
+    return (
+        <AlertDialog open={open} onOpenChange={() => setOpen(!open)}>
+            {children ? children : (
+                <AlertDialogTrigger asChild>
+                    <Button variant="outline">Show Dialog</Button>
+                </AlertDialogTrigger>
+            )}
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>{dialogProps.title}</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        {dialogProps.description}
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction onClick={dialogProps.onContinue}>
+                        {buttonName}
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    );
+})
+
+export default ConfirmAlert;
