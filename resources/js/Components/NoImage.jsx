edf9345b300e66e6b827e792b2f9e4095dcb095c
Donz <donnygapulao@gmail.com>
2025-07-04 16:46:46 +0800

    added onClick handler on No Image component

--- resources/js/Components/NoImage.jsx
+++ resources/js/Components/NoImage.jsx
@@ -1,6 +1,6 @@
-export default function NoImage({ text = 'No Image', height = '300px', width ='300px' }) {
+export default function NoImage({ text = 'No Image', height = '300px', width ='300px', onClick = null }) {
     return (
-        <div style={{ height, width }} className="bg-gray-200 rounded-md flex items-center justify-center">
+        <div style={{ height, width }} className="bg-gray-200 rounded-md flex items-center justify-center" onClick={onClick}>
             <p className="text-gray-500 text-2xl font-medium">{ text }</p>
         </div>
     );
