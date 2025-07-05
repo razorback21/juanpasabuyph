5c6b978f9e173ef792bfe10abb596dc80efa1310
Donz <donnygapulao@gmail.com>
2025-07-05 14:15:53 +0800

    added service convert to webp

---
+++ app/Services/ConventToWebp.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Services;
+
+use Buglinjo\LaravelWebp\Facades\Webp;
+
+class ConventToWebp
+{
+    public function convert($file, $storage_folder = "", $quality = 70)
+    {
+        $path = "storage/{$storage_folder}/" . pathinfo($file->hashName(), PATHINFO_FILENAME) . ".webp";
+        $fileName = public_path($path);
+        if (Webp::make($file)->save($fileName, $quality)) {
+            return "/{$path}";
+        } else {
+            throw new \Exception("Failed to upload image");
+        }
+    }
+}
