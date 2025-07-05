aba78060f9c3e05b6ebde3eb1e3f6bb90ed189a8
Donz <donnygapulao@gmail.com>
2025-07-05 09:08:04 +0800

    refector code

--- app/Http/Controllers/ProductImageController.php
+++ app/Http/Controllers/ProductImageController.php
@@ -2,39 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductImageUploadRequest;
 use App\Models\Product;
+use App\Services\FileUploadService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class ProductImageController extends Controller
 {
-    
-    public function uploadFeaturedImage(Request $request, $id)
-    {
-        $validator = Validator::make($request->all(), [
-            'featured_image' => 'required|image|mimes:jpeg,png,jpg,webp|max:2048',
-        ]);
 
+    public function __construct(protected FileUploadService $fileUploadService)
+    {
+
+    }
+
+    public function upload(ProductImageUploadRequest $request, $id)
+    {
+        $validator = $request->validated();
         if ($validator->fails()) {
             return back()->withErrors($validator)->withInput();
         }
 
         $product = Product::findOrFail($id);
         
-        // Delete old image if exists
-        if ($product->featured_image) {
-            Storage::disk('public')->delete($product->featured_image);
-        }
-        
-        // Store new image
-        $imagePath = $request->file('featured_image')->store('products', 'public');
+        // Method 1: Using the specific featured image method
+        $imagePath = $this->fileUploadService->uploadImageByType($product, $request->file('featured_image'),$request->input('type'));
         
         // Update product with new image path
         $product->update([
             'featured_image' => '/storage/' . $imagePath
         ]);
-        
-        return back()->with('success', 'Product image updated successfully!');
+
+        return back()->with('success', 'Product featured image updated successfully!');
     }
 }
