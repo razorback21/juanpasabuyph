334dee8850414aaa3c45eade8d7285be304cc6d0
Donz <donnygapulao@gmail.com>
2025-07-12 14:02:05 +0800

    added getCSVStatus

--- app/Enums/OrderStatusEnum.php
+++ app/Enums/OrderStatusEnum.php
@@ -8,4 +8,10 @@ enum OrderStatusEnum: string
     case PROCESSING = 'processing';
     case SHIPPED = 'shipped';
     case CANCELLED = 'cancelled';
+
+    public static function getCSVStatus(): string
+    {
+        return implode(',', array_column(self::cases(), 'value'));
+    }
+
 }
