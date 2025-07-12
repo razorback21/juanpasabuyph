f9fe5dbaccb9e1e3efc468923305d36d64842a15
Donz <donnygapulao@gmail.com>
2025-07-12 11:42:54 +0800

    badge status color

--- resources/js/Pages/Products/InventoryTable.jsx
+++ resources/js/Pages/Products/InventoryTable.jsx
@@ -10,8 +10,21 @@ import {
     TableRow,
 } from "@/components/ui/table";
 
-
 function InventoryTable({ inventory }) {
+    function movementTypeStatusColor(movementType) {
+        switch (movementType) {
+            case "inbound":
+                return "bg-green-400 text-white";
+            case "outbound":
+                return "bg-red-400 text-white";
+            case "reserved":
+                return "bg-orange-400 text-white";
+            case "returned":
+                return "bg-blue-400 text-white";
+            default:
+                return "";
+        }
+    }
     return inventory.length === 0 ? (
         <p className="text-sm">No inventory found.</p>
     ) : (
@@ -30,10 +43,19 @@ function InventoryTable({ inventory }) {
                 {inventory.map((data) => (
                     <TableRow key={data.invoice}>
                         <TableCell className="font-medium">
-                            <Badge variant='secondary'>{data.movement_type.toString().toUpperCase()}</Badge>
+                            <Badge
+                                variant="secondary"
+                                className={movementTypeStatusColor(
+                                    data.movement_type.toString().toLowerCase()
+                                )}
+                            >
+                                {data.movement_type.toString().toUpperCase()}
+                            </Badge>
                         </TableCell>
                         <TableCell>{data.quantity}</TableCell>
-                        <TableCell>{data.uom.toString().toUpperCase()}</TableCell>
+                        <TableCell>
+                            {data.uom.toString().toUpperCase()}
+                        </TableCell>
                         <TableCell>{data.notes}</TableCell>
                         <TableCell>{data.order_id}</TableCell>
                         <TableCell>
