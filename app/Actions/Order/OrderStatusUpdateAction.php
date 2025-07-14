d48aa7f529086ee3a26ebfc0bd9583b1a323f8bb
Donz <donnygapulao@gmail.com>
2025-07-14 11:40:13 +0800

    added OrderStatusUpdate action

---
+++ app/Actions/Order/OrderStatusUpdateAction.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Actions\Order;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class OrderStatusUpdateAction
+{
+    use AsAction;
+
+    public function handle()
+    {
+        // ...
+    }
+}
