d9c79ce7378a671fb07aabed074c1975c2bc3ab0
Donz <donnygapulao@gmail.com>
2025-07-14 15:31:11 +0800

    removed net price

--- database/migrations/2025_06_12_002525_create_order_items_table.php
+++ database/migrations/2025_06_12_002525_create_order_items_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,7 +16,6 @@ return new class extends Migration
             $table->foreignId('product_id')->constrained()->onDelete('restrict');
             $table->integer('quantity');
             $table->decimal('price', 10, 2);
-            $table->decimal('net_price', 10, 2);
             $table->timestamps();
         });
     }
