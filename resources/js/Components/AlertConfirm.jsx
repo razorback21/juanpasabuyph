ba015febb1c54822c4375bdd244c9fc40e7918bc
Donz <donnygapulao@gmail.com>
2025-07-17 17:10:13 +0800

    updaetd alert confirm

--- resources/js/Components/AlertConfirm.jsx
+++ resources/js/Components/AlertConfirm.jsx
@@ -11,48 +11,55 @@ import {
 } from "@/components/ui/alert-dialog";
 import { AlertDialogTrigger } from "@radix-ui/react-alert-dialog";
 
-const AlertConfirm = forwardRef(({ buttonName = 'Continue' }, ref) => {
+const AlertConfirm = forwardRef(({ buttonName = "Continue" }, ref) => {
     const [open, setOpen] = useState(false);
     const [dialogProps, setDialogProps] = useState({
-        title: 'Message',
-        description:'',
+        title: "Message",
+        description: "",
         onContinue: () => {},
-        footer: true
+        footer: true,
+        buttonName: "",
     });
     const { title, description, onContinue, footer } = dialogProps;
 
     useImperativeHandle(ref, () => ({
-        open(props) {    
+        open(props) {
             setDialogProps(props);
             setOpen(true);
         },
         close() {
             setOpen(false);
-        }
+        },
     }));
     return (
-        <AlertDialog open={open} onOpenChange={() => setOpen(!open)} onInteractOutside={(e) => setOpen(false)}>
+        <AlertDialog
+            open={open}
+            onOpenChange={() => setOpen(!open)}
+            onInteractOutside={(e) => setOpen(false)}
+        >
             <AlertDialogContent>
                 <AlertDialogHeader>
                     <AlertDialogTitle>{title}</AlertDialogTitle>
                     <AlertDialogDescription>
                         {description}
                     </AlertDialogDescription>
-                    
                 </AlertDialogHeader>
-               
-                    <AlertDialogFooter>
-                        <AlertDialogCancel>{ !onContinue? 'Close' : 'Cancel'}</AlertDialogCancel>
-                        { onContinue && (
-                            <AlertDialogAction onClick={onContinue}>
-                                {buttonName}
-                            </AlertDialogAction>
-                        )}
-                    </AlertDialogFooter>
-                
+
+                <AlertDialogFooter>
+                    <AlertDialogCancel>
+                        {!onContinue ? "Close" : "Cancel"}
+                    </AlertDialogCancel>
+                    {onContinue && (
+                        <AlertDialogAction onClick={onContinue}>
+                            {dialogProps.buttonName != ""
+                                ? dialogProps.buttonName
+                                : buttonName}
+                        </AlertDialogAction>
+                    )}
+                </AlertDialogFooter>
             </AlertDialogContent>
         </AlertDialog>
     );
-})
+});
 
 export default AlertConfirm;
