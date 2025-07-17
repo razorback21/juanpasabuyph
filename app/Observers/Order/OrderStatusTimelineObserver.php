b5d0eab37d5da764a5bcdc5a9b6f0d4fd7efbb5e
Donz <donnygapulao@gmail.com>
2025-07-17 19:03:26 +0800

    added OrderStatusTimelineObserver

---
+++ app/Observers/Order/OrderStatusTimelineObserver.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Observers\Order;
+
+use App\Enums\OrderStatusEnum;
+use App\Events\EventOrderStatusChanged;
+use App\Models\OrderStatusTimeline;
+
+class OrderStatusTimelineObserver
+{
+    /**
+     * Handle the OrderStatusTimeline "created" event.
+     */
+    public function created(OrderStatusTimeline $orderStatusTimeline): void
+    {
+        event(new EventOrderStatusChanged($orderStatusTimeline->order, $orderStatusTimeline->status));
+    }
+
+    /**
+     * Handle the OrderStatusTimeline "updated" event.
+     */
+    public function updated(OrderStatusTimeline $orderStatusTimeline): void
+    {
+        event(new EventOrderStatusChanged($orderStatusTimeline->order, $orderStatusTimeline->status));
+    }
+
+    /**
+     * Handle the OrderStatusTimeline "deleted" event.
+     */
+    public function deleted(OrderStatusTimeline $orderStatusTimeline): void
+    {
+        //
+    }
+
+    /**
+     * Handle the OrderStatusTimeline "restored" event.
+     */
+    public function restored(OrderStatusTimeline $orderStatusTimeline): void
+    {
+        //
+    }
+
+    /**
+     * Handle the OrderStatusTimeline "force deleted" event.
+     */
+    public function forceDeleted(OrderStatusTimeline $orderStatusTimeline): void
+    {
+        //
+    }
+}
