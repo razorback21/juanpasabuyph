ea5fe2dbaa895ca4e5c59b9f6bb59d6d1cfc8b17
Donz <donnygapulao@gmail.com>
2025-07-19 15:09:42 +0800

    order statuschange listner

--- app/Listeners/OrderStatusChanged.php
+++ app/Listeners/OrderStatusChanged.php
@@ -5,6 +5,8 @@ namespace App\Listeners;
 use App\Enums\OrderStatusEnum;
 use App\Events\EventOrderStatusChanged;
 use App\Models\Order;
+use App\Models\OrderStatusTimeline;
+use App\Notifications\OrderStatusChangedNotification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
@@ -27,5 +29,35 @@ class OrderStatusChanged
         $event->order->update([
             'status' => $event->orderStatusEnum,
         ]);
+
+        $this->handleReservedItems($event->order, $event->orderStatusEnum);
+        $this->handleOrderStatusNotifications($event->order, $event->orderStatusEnum);
     }
+
+    protected function handleReservedItems(Order $order, OrderStatusEnum $status)
+    {
+        if (OrderStatusEnum::CANCELLED->value == $status->value) {
+            $order->inventoryReservations()->delete();
+        }
+    }
+
+    protected function handleOrderStatusNotifications(Order $order, OrderStatusEnum $orderStatus)
+    {
+        $timelineMessage = '';
+        $timeline = $order->timeline()->where('status', '=', $orderStatus->value)->first();
+
+        if ($timeline) {
+            $timelineMessage = $timeline->description;
+        }
+
+        $message = [
+            OrderStatusEnum::CANCELLED->value => 'Sorry, your order has been cancelled. ' . $timelineMessage,
+            OrderStatusEnum::PROCESSING->value => 'Your order is now being processed. ' . $timelineMessage,
+            OrderStatusEnum::SHIPPED->value => 'Your order has been shipped. ' . $timelineMessage,
+        ];
+        if (isset($message[$orderStatus->value])) {
+            $order->customer->notify(new OrderStatusChangedNotification($order, $message[$orderStatus->value]));
+        }
+    }
+
 }
