7058c864ef24c14692eb9b996ec4947bcd3864f9
Donz <donnygapulao@gmail.com>
2025-07-23 22:01:45 +0800

    initial code

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -4,14 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CatalogController extends Controller
 {
     public function index(Request $user)
     {
-        $products = Product::query()
-            ->with('productCategory')
-            ->get();
+        $products = Product::all();
+
         return Inertia::render("Store/Catalog/Index", [
             'title' => "Catalog",
             'products' => $products
