f35d10b92432d7a1f1c1ae5d7bf416323f0f35fc
Donz <donnygapulao@gmail.com>
2025-07-23 22:45:53 +0800

    import Link

--- resources/js/Pages/Store/Catalog/index.jsx
+++ resources/js/Pages/Store/Catalog/index.jsx
@@ -2,6 +2,7 @@ import Layout from "@/Pages/Store/components/Layout.jsx";
 import HomeProducts from "../components/HomeProducts";
 import Reviews from "../components/Reviews";
 import productsData from "@/data/products.json";
+import { Link } from "@inertiajs/react";
 
 export default function ({ title }) {
     const products = productsData;
@@ -114,9 +115,12 @@ export default function ({ title }) {
                                 >
                                     <path d="M222.14,58.87A8,8,0,0,0,216,56H54.68L49.79,29.14A16,16,0,0,0,34.05,16H16a8,8,0,0,0,0,16h18L59.56,172.29a24,24,0,0,0,5.33,11.27,28,28,0,1,0,44.4,8.44h45.42A27.75,27.75,0,0,0,152,204a28,28,0,1,0,28-28H83.17a8,8,0,0,1-7.87-6.57L72.13,152h116a24,24,0,0,0,23.61-19.71l12.16-66.86A8,8,0,0,0,222.14,58.87ZM96,204a12,12,0,1,1-12-12A12,12,0,0,1,96,204Zm96,0a12,12,0,1,1-12-12A12,12,0,0,1,192,204Zm4-74.57A8,8,0,0,1,188.1,136H69.22L57.59,72H206.41Z"></path>
                                 </svg>
-                                <span className="transition-all duration-300">
+                                <Link
+                                    href={`/catalog/${product.category.slug}/${product.slug}`}
+                                    className="transition-all duration-300"
+                                >
                                     Buy Now
-                                </span>
+                                </Link>
                             </button>
                         </div>
                     </div>
