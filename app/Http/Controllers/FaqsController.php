facdcc5b20d6710ef4ab2d4f4b375380b880f9c7
Donz <donnygapulao@gmail.com>
2025-07-25 14:12:59 +0800

    faqs index controlleradded index controller

--- app/Http/Controllers/FaqsController.php
+++ app/Http/Controllers/FaqsController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class FaqsController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('Store/Faqs/Index');
+    }
 }
