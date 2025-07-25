723848a15cd13e4df36188dcf1a2692eb77e3fec
Donz <donnygapulao@gmail.com>
2025-07-25 15:49:00 +0800

    contact controler and layout

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ContactController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('Store/Contact/Index');
+    }
 }
