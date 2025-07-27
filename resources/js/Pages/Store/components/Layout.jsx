6b0745ef1d86563101bbf3ea357fea60d5da1e45
Donz <donnygapulao@gmail.com>
2025-07-27 18:15:33 +0800

    added new page props

--- resources/js/Pages/Store/components/Layout.jsx
+++ resources/js/Pages/Store/components/Layout.jsx
@@ -1,9 +1,10 @@
 import { Head } from "@inertiajs/react";
 import Footer from "./Footer";
 import Logo from "./Logo";
-import { Link } from "@inertiajs/react";
+import { Link, usePage } from "@inertiajs/react";
 
 export default function ({ title, children }) {
+    const props = usePage().props;
     const LinksData = [
         {
             name: "Home",
@@ -66,7 +67,7 @@ export default function ({ title, children }) {
                                     <path d="M222.14,58.87A8,8,0,0,0,216,56H54.68L49.79,29.14A16,16,0,0,0,34.05,16H16a8,8,0,0,0,0,16h18L59.56,172.29a24,24,0,0,0,5.33,11.27,28,28,0,1,0,44.4,8.44h45.42A27.75,27.75,0,0,0,152,204a28,28,0,1,0,28-28H83.17a8,8,0,0,1-7.87-6.57L72.13,152h116a24,24,0,0,0,23.61-19.71l12.16-66.86A8,8,0,0,0,222.14,58.87ZM96,204a12,12,0,1,1-12-12A12,12,0,0,1,96,204Zm96,0a12,12,0,1,1-12-12A12,12,0,0,1,192,204Zm4-74.57A8,8,0,0,1,188.1,136H69.22L57.59,72H206.41Z"></path>
                                 </svg>
                                 <span class="absolute -top-1 -right-1 flex h-4 w-4 items-center justify-center rounded-full bg-[#e92933] text-xs font-bold text-white">
-                                    3
+                                    {props.cartCount}
                                 </span>
                             </button>
                         </div>
