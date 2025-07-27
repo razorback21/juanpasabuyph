6e5fdd42d2d7bcdb70976a7e3709d80b4e0bb348
Donz <donnygapulao@gmail.com>
2025-07-27 18:14:47 +0800

    added cart count

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Services\CartService;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 use Tighten\Ziggy\Ziggy;
@@ -42,7 +43,7 @@ class HandleInertiaRequests extends Middleware
             'flash' => [
                 'message' => fn() => $request->session()->get('message')
             ],
-
+            'cartCount' => fn() => (new CartService())->getCartCount(),
         ];
     }
 }
