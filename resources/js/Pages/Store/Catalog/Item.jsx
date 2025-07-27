20fce8b85caa9d1a377c04f614e1302c0020f85f
Donz <donnygapulao@gmail.com>
2025-07-27 17:58:03 +0800

    added addt o cart initial code

--- resources/js/Pages/Store/Catalog/Item.jsx
+++ resources/js/Pages/Store/Catalog/Item.jsx
@@ -1,12 +1,40 @@
 import Layout from "@/Pages/Store/components/Layout";
 import { Badge } from "@/components/ui/Badge";
 import HomeProducts from "../components/HomeProducts";
+import QuantityInput from "../components/QuantityInput";
+import { router, usePage } from "@inertiajs/react";
+import { Toaster } from "@/Components/ui/sonner";
+import { toast } from "sonner";
+import { useRef } from "react";
 
 export default function ({ title, product, category, relatedProducts }) {
-    console.log(relatedProducts);
+    const props = usePage().props;
+    const quantityInputRef = useRef(null);
+    console.log(props);
+    const handlerAddToCart = (e) => {
+        e.preventDefault();
+        const quantity = quantityInputRef.current.getQuantity();
+        router.put(
+            route("cart.update"),
+            {
+                product_id: product.id,
+                quantity,
+            },
+            {
+                onSuccess: (response) => {
+                    if (props.flash.message) {
+                        toast.success(props.flash.message);
+                    } else {
+                        toast.success("Item added to cart 121");
+                    }
+                },
+            }
+        );
+    };
 
     return (
         <Layout title={title} category={category}>
+            <Toaster />
             <div className="mb-6 text-sm text-[#6b7280]">
                 <a className="hover:text-[#e92933] transition-colors" href="#">
                     {category}
@@ -65,24 +93,15 @@ export default function ({ title, product, category, relatedProducts }) {
                     </div>
 
                     <div className="mb-6">
-                        <h3 className="text-sm font-semibold text-[#1f2937] mb-2">
-                            Quantity:
-                        </h3>
-                        <div className="flex items-center">
-                            <button className="flex items-center justify-center h-10 w-10 rounded-l-md border border-r-0 border-[#d1d5db] text-[#4b5563] hover:bg-[#f3f4f6] transition-colors">
-                                -
-                            </button>
-                            <input
-                                className="form-input h-10 w-16 text-center border-y border-x-0 border-[#d1d5db] text-[#1f2937] focus:outline-none focus:ring-2 focus:ring-[#e92933] focus:border-[#e92933]"
-                                type="number"
-                                defaultValue="1"
-                            />
-                            <button className="flex items-center justify-center h-10 w-10 rounded-r-md border border-l-0 border-[#d1d5db] text-[#4b5563] hover:bg-[#f3f4f6] transition-colors">
-                                +
-                            </button>
-                        </div>
+                        <QuantityInput ref={quantityInputRef} />
                     </div>
-                    <button className="add-to-cart-button w-full flex items-center justify-center gap-2 rounded-lg h-12 px-6 bg-[#e92933] text-white text-base font-semibold hover:bg-red-700 transition-colors shadow-md hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-[#e92933] focus:ring-opacity-50">
+                    {props.errors.quantity && (
+                        <p className="text-sm my-2">{props.errors.quantity}</p>
+                    )}
+                    <button
+                        onClick={handlerAddToCart}
+                        className="add-to-cart-button w-full flex items-center justify-center gap-2 rounded-lg h-12 px-6 bg-[#e92933] text-white text-base font-semibold hover:bg-red-700 transition-colors shadow-md hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-[#e92933] focus:ring-opacity-50"
+                    >
                         <svg
                             fill="currentColor"
                             height="20px"
@@ -93,7 +112,7 @@ export default function ({ title, product, category, relatedProducts }) {
                             <path d="M222.14,58.87A8,8,0,0,0,216,56H54.68L49.79,29.14A16,16,0,0,0,34.05,16H16a8,8,0,0,0,0,16h18L59.56,172.29a24,24,0,0,0,5.33,11.27,28,28,0,1,0,44.4,8.44h45.42A27.75,27.75,0,0,0,152,204a28,28,0,1,0,28-28H83.17a8,8,0,0,1-7.87-6.57L72.13,152h116a24,24,0,0,0,23.61-19.71l12.16-66.86A8,8,0,0,0,222.14,58.87ZM96,204a12,12,0,1,1-12-12A12,12,0,0,1,96,204Zm96,0a12,12,0,1,1-12-12A12,12,0,0,1,192,204Zm4-74.57A8,8,0,0,1,188.1,136H69.22L57.59,72H206.41Z"></path>
                         </svg>
                         <span className="add-to-cart-text">Add to Cart</span>
-                        <span className="add-to-cart-text-hover">Added!</span>
+                        {/* <span className="add-to-cart-text-hover">Added!</span> */}
                     </button>
                 </div>
             </div>
