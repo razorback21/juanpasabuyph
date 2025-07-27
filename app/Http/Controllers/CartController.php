cb17a82572b38e071cef26ad91910ca7fe50e22d
Donz <donnygapulao@gmail.com>
2025-07-27 18:15:15 +0800

    added update code

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,27 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\Customer;
 use App\Models\Order;
+use App\Rules\ProductStock;
+use App\Services\CartService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    public function store(Request $request)
+    public function __construct(public CartService $cartService)
     {
-        // Save order to session ['id' =>1, 'quantity' => 10]
-        $order = $request->session()->get('order', []);
-        $order[$request->input('id')] = $request->input('quantity');
-        $request->session()->put('order', $order);
-
-        return redirect()->route('cart.checkout')->with('message', 'Item added to cart');
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
-        $order = $request->session()->get('order', []);
-        $order[$id] = $request->input('quantity');
-        $request->session()->put('order', $order);
+        $validated = $request->validate([
+            'product_id' => 'required|integer|exists:products,id',
+            'quantity' => ['required', 'integer', 'min:1', new ProductStock($request->post('product_id'))],
+        ]);
 
-        return redirect()->route('cart.checkout');
+        $this->cartService->addItem($validated['product_id'], $validated['quantity']);
+
+        return redirect()->back()->with('message', 'Item added to cart');
     }
 
     public function destroy(Request $request, $id)
