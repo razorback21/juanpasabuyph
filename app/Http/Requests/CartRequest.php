a457bee29725ba8c61b31f5eb5a32071f308b1f0
Donz <donnygapulao@gmail.com>
2025-07-28 14:43:02 +0800

    added custom request for cart

---
+++ app/Http/Requests/CartRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\ProductStock;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CartRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'product_id' => 'required|integer|exists:products,id',
+            'quantity' => ['required', 'integer', 'min:1', new ProductStock($this->post('product_id'))],
+        ];
+    }
+}
