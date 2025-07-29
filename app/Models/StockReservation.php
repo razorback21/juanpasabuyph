46e12ee954d457b46d3c7af2b9482663c1ed246c
Donz <donnygapulao@gmail.com>
2025-07-29 14:05:53 +0800

    stock reservation model

--- app/Models/StockReservation.php
+++ app/Models/StockReservation.php
@@ -3,20 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class StockReservation extends Model
 {
     protected $guarded = [
-        create_at,
-        update_at,
+        'created_at',
+        'updated_at',
     ];
 
-    public function order()
+    public function order(): BelongsTo
     {
         return $this->belongsTo(Order::class);
     }
 
-    public function product()
+    public function product(): BelongsTo
     {
         return $this->belongsTo(Product::class);
     }
