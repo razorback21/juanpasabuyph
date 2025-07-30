21c79a6ec6186a0d69ca8c9719362bef4924290a
Donz <donnygapulao@gmail.com>
2025-07-30 12:07:05 +0800

    cleanout dashboard

--- resources/js/Pages/Dashboard/Index.jsx
+++ resources/js/Pages/Dashboard/Index.jsx
@@ -1,21 +1,11 @@
 //import { DataTable } from "@/components/data-table"
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
-import ProductsTable from "./ProductsTable";
-import Pagination from "@/Components/Pagination";
 
 export default function (props) {
     return (
-        <AuthenticatedLayout header='Products'>
+        <AuthenticatedLayout header="Dashboard">
             <Head title="Dashboard" />
-            <div className="flex flex-col px-4 lg:px-6">
-                <ProductsTable
-                   {...props}
-                />
-            </div>
-            <div className="text-gray-900 text-center">
-                <Pagination links={props.products.links} />
-            </div>
         </AuthenticatedLayout>
     );
 }
