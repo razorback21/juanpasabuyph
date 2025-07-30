756cdbe9e415fa3de395975ebbb152040cddb281
Donz <donnygapulao@gmail.com>
2025-07-30 11:26:13 +0800

    menu

--- resources/js/Components/app-sidebar.jsx
+++ resources/js/Components/app-sidebar.jsx
@@ -1,187 +1,221 @@
-import * as React from "react"
-import { usePage, Link } from "@inertiajs/react"
+import * as React from "react";
+import { usePage, Link } from "@inertiajs/react";
 import {
-  ArrowUpCircleIcon,
-  BarChartIcon,
-  CameraIcon,
-  ClipboardListIcon,
-  CodesandboxIcon,
-  CodeSquare,
-  DatabaseIcon,
-  FileCodeIcon,
-  FileIcon,
-  FileTextIcon,
-  FolderIcon,
-  FoldersIcon,
-  HelpCircleIcon,
-  LayoutDashboardIcon,
-  ListIcon,
-  Package2Icon,
-  Rows2Icon,
-  SearchIcon,
-  SettingsIcon,
-  UsersIcon,
-} from "lucide-react"
+    ArrowUpCircleIcon,
+    BarChartIcon,
+    CameraIcon,
+    ClipboardListIcon,
+    CodesandboxIcon,
+    CodeSquare,
+    DatabaseIcon,
+    FileCodeIcon,
+    FileIcon,
+    FileTextIcon,
+    FolderIcon,
+    FoldersIcon,
+    HelpCircleIcon,
+    LayoutDashboardIcon,
+    ListIcon,
+    Package2Icon,
+    Rows2Icon,
+    SearchIcon,
+    SettingsIcon,
+    UsersIcon,
+} from "lucide-react";
 
-import { NavDocuments } from "@/components/nav-documents"
-import { NavMain } from "@/components/nav-main"
-import { NavSecondary } from "@/components/nav-secondary"
-import { NavUser } from "@/components/nav-user"
+import { NavDocuments } from "@/components/nav-documents";
+import { NavMain } from "@/components/nav-main";
+import { NavSecondary } from "@/components/nav-secondary";
+import { NavUser } from "@/components/nav-user";
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "@/components/ui/sidebar"
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "@/components/ui/sidebar";
 
 const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
-  navMain: [
-    {
-      title: "Products",
-      url: "#",
-      route: "dashboard",
-      icon: Rows2Icon,
-      activeKey: 'dashboard',
+    user: {
+        name: "shadcn",
+        email: "m@example.com",
+        avatar: "/avatars/shadcn.jpg",
     },
+    navMain: [
         {
-      title: "Categories",
-      url: "#",
-      route: 'product-categories.index',
-      icon: FoldersIcon,
-      activeKey: 'product-categories',
-    },
-    {
-      title: "Orders",
-      url:'#',
-      route: "orders.index",
-      icon: Package2Icon,
-      activeKey: 'order',
-    },
-    // {
-    //   title: "Customers",
-    //   url: "#",
-    //   icon: UsersIcon,
-    // },
-  ],
-  navClouds: [
-    {
-      title: "Capture",
-      icon: CameraIcon,
-      isActive: true,
-      url: "#",
-      items: [
+            title: "Dashboard",
+            url: "#",
+            route: "dashboard",
+            icon: LayoutDashboardIcon,
+            activeKey: "dashboard",
+        },
+        // {
+        //   title: "Customers",
+        //   url: "#",
+        //   icon: UsersIcon,
+        // },
+    ],
+    navClouds: [
         {
-          title: "Active Proposals",
-          url: "#",
+            title: "Capture",
+            icon: CameraIcon,
+            isActive: true,
+            url: "#",
+            items: [
+                {
+                    title: "Active Proposals",
+                    url: "#",
+                },
+                {
+                    title: "Archived",
+                    url: "#",
+                },
+            ],
         },
         {
-          title: "Archived",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Proposal",
-      icon: FileTextIcon,
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
-        },
-        {
-          title: "Archived",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Prompts",
-      icon: FileCodeIcon,
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
+            title: "Proposal",
+            icon: FileTextIcon,
+            url: "#",
+            items: [
+                {
+                    title: "Active Proposals",
+                    url: "#",
+                },
+                {
+                    title: "Archived",
+                    url: "#",
+                },
+            ],
         },
         {
-          title: "Archived",
-          url: "#",
+            title: "Prompts",
+            icon: FileCodeIcon,
+            url: "#",
+            items: [
+                {
+                    title: "Active Proposals",
+                    url: "#",
+                },
+                {
+                    title: "Archived",
+                    url: "#",
+                },
+            ],
         },
-      ],
-    },
-  ],
-  navSecondary: [
-    {
-      title: "Settings",
-      url: "#",
-      icon: SettingsIcon,
-    },
-    {
-      title: "Get Help",
-      url: "#",
-      icon: HelpCircleIcon,
-    },
-    {
-      title: "Search",
-      url: "#",
-      icon: SearchIcon,
-    },
-  ],
-  documents: [
-    {
-      name: "Data Library",
-      url: "#",
-      icon: DatabaseIcon,
-    },
-    {
-      name: "Reports",
-      url: "#",
-      icon: ClipboardListIcon,
-    },
-    {
-      name: "Word Assistant",
-      url: "#",
-      icon: FileIcon,
-    },
-  ],
-}
+    ],
+    navSecondary: [
+        {
+            title: "Settings",
+            url: "#",
+            icon: SettingsIcon,
+        },
+        {
+            title: "Get Help",
+            url: "#",
+            icon: HelpCircleIcon,
+        },
+        {
+            title: "Search",
+            url: "#",
+            icon: SearchIcon,
+        },
+    ],
+    inventory: [
+        {
+            name: "Products",
+            url: "#",
+            route: "dashboard",
+            icon: Rows2Icon,
+            activeKey: "dashboard",
+        },
+        {
+            name: "Categories",
+            url: "#",
+            route: "product-categories.index",
+            icon: FoldersIcon,
+            activeKey: "product-categories",
+        },
+        {
+            name: "Featured Products",
+            url: "#",
+            route: "product-categories.index",
+            icon: FoldersIcon,
+            activeKey: "product-categories",
+        },
+        // {
+        //     name: "Data Library",
+        //     url: "#",
+        //     icon: DatabaseIcon,
+        // },
+        // {
+        //     name: "Reports",
+        //     url: "#",
+        //     icon: ClipboardListIcon,
+        // },
+        // {
+        //     name: "Word Assistant",
+        //     url: "#",
+        //     icon: FileIcon,
+        // },
+    ],
+    reports: [
+        {
+            name: "Orders",
+            url: "#",
+            route: "orders.index",
+            icon: Package2Icon,
+            activeKey: "order",
+        },
+        {
+            name: "Out of Stock",
+            url: "#",
+            route: "orders.index",
+            icon: Package2Icon,
+            activeKey: "order",
+        },
+        {
+            name: "Disabled Products",
+            url: "#",
+            route: "orders.index",
+            icon: Package2Icon,
+            activeKey: "order",
+        },
+    ],
+};
 
-export function AppSidebar({
-  ...props
-}) {
-  const inertiaProps = usePage().props;
-  console.log(inertiaProps);
-  return (
-    <Sidebar collapsible="offcanvas" {...props}>
-      <SidebarHeader>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton asChild className="data-[slot=sidebar-menu-button]:!p-1.5">
-              <Link href="/">
-                <ArrowUpCircleIcon className="h-5 w-5" />
-                <span className="text-base font-semibold">JUANPSABUYPH</span>
-              </Link>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarHeader>
-      <SidebarContent>
-        <NavMain items={data.navMain} />
-        {/* <NavDocuments items={data.documents} /> */}
-        {/* <NavSecondary items={data.navSecondary} className="mt-auto" /> */}
-      </SidebarContent>
-      <SidebarFooter>
-        <NavUser user={inertiaProps.auth.user} />
-      </SidebarFooter>
-    </Sidebar>
-  );
+export function AppSidebar({ ...props }) {
+    const inertiaProps = usePage().props;
+    console.log(inertiaProps);
+    return (
+        <Sidebar collapsible="offcanvas" {...props}>
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton
+                            asChild
+                            className="data-[slot=sidebar-menu-button]:!p-1.5"
+                        >
+                            <Link href="/">
+                                <ArrowUpCircleIcon className="h-5 w-5" />
+                                <span className="text-base font-semibold">
+                                    JUANPSABUYPH
+                                </span>
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+            <SidebarContent>
+                <NavMain items={data.navMain} />
+                <NavDocuments items={data.inventory} menuName="Inventory" />
+                <NavDocuments items={data.reports} menuName="Reports" />
+                {/* <NavSecondary items={data.navSecondary} className="mt-auto" /> */}
+            </SidebarContent>
+            <SidebarFooter>
+                <NavUser user={inertiaProps.auth.user} />
+            </SidebarFooter>
+        </Sidebar>
+    );
 }
--- resources/js/Components/nav-documents.jsx
+++ resources/js/Components/nav-documents.jsx
@@ -1,70 +1,73 @@
-"use client"
+"use client";
 
+import { Link } from "@inertiajs/react";
 import { FolderIcon, MoreHorizontalIcon, ShareIcon } from "lucide-react";
 
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import {
-  SidebarGroup,
-  SidebarGroupLabel,
-  SidebarMenu,
-  SidebarMenuAction,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  useSidebar,
-} from "@/components/ui/sidebar"
+    SidebarGroup,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuAction,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    useSidebar,
+} from "@/components/ui/sidebar";
 
-export function NavDocuments({
-  items
-}) {
-  const { isMobile } = useSidebar()
+export function NavDocuments({ items, menuName = "Documents" }) {
+    const { isMobile } = useSidebar();
 
-  return (
-    <SidebarGroup className="group-data-[collapsible=icon]:hidden">
-      <SidebarGroupLabel>Documents</SidebarGroupLabel>
-      <SidebarMenu>
-        {items.map((item) => (
-          <SidebarMenuItem key={item.name}>
-            <SidebarMenuButton asChild>
-              <a href={item.url}>
-                <item.icon />
-                <span>{item.name}</span>
-              </a>
-            </SidebarMenuButton>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <SidebarMenuAction showOnHover className="rounded-sm data-[state=open]:bg-accent">
-                  <MoreHorizontalIcon />
-                  <span className="sr-only">More</span>
-                </SidebarMenuAction>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent
-                className="w-24 rounded-lg"
-                side={isMobile ? "bottom" : "right"}
-                align={isMobile ? "end" : "start"}>
-                <DropdownMenuItem>
-                  <FolderIcon />
-                  <span>Open</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <ShareIcon />
-                  <span>Share</span>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </SidebarMenuItem>
-        ))}
-        <SidebarMenuItem>
-          <SidebarMenuButton className="text-sidebar-foreground/70">
-            <MoreHorizontalIcon className="text-sidebar-foreground/70" />
-            <span>More</span>
-          </SidebarMenuButton>
-        </SidebarMenuItem>
-      </SidebarMenu>
-    </SidebarGroup>
-  );
+    return (
+        <SidebarGroup className="group-data-[collapsible=icon]:hidden">
+            <SidebarGroupLabel>{menuName}</SidebarGroupLabel>
+            <SidebarMenu>
+                {items.map((item) => (
+                    <SidebarMenuItem key={item.name}>
+                        <SidebarMenuButton asChild>
+                            <Link href={route(item.route)}>
+                                <item.icon />
+                                <span>{item.name}</span>
+                            </Link>
+                        </SidebarMenuButton>
+                        {/* <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <SidebarMenuAction
+                                    showOnHover
+                                    className="rounded-sm data-[state=open]:bg-accent"
+                                >
+                                    <MoreHorizontalIcon />
+                                    <span className="sr-only">More</span>
+                                </SidebarMenuAction>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent
+                                className="w-24 rounded-lg"
+                                side={isMobile ? "bottom" : "right"}
+                                align={isMobile ? "end" : "start"}
+                            >
+                                <DropdownMenuItem>
+                                    <FolderIcon />
+                                    <span>Open</span>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                    <ShareIcon />
+                                    <span>Share</span>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu> */}
+                    </SidebarMenuItem>
+                ))}
+                {/* <SidebarMenuItem>
+                    <SidebarMenuButton className="text-sidebar-foreground/70">
+                        <MoreHorizontalIcon className="text-sidebar-foreground/70" />
+                        <span>More</span>
+                    </SidebarMenuButton>
+                </SidebarMenuItem> */}
+            </SidebarMenu>
+        </SidebarGroup>
+    );
 }
