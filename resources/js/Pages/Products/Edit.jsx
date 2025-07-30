84cf698e8a2d6dbebaffdff87005881889a5bda6
Donz <donnygapulao@gmail.com>
2025-07-30 12:00:46 +0800

    move products to its specific folder

--- resources/js/Pages/Products/Edit.jsx
+++ resources/js/Pages/Products/Edit.jsx
@@ -112,7 +112,7 @@ export default function EditProduct({ product, categories, from }) {
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 bg-white">
                     <div className="text-right pt-4">
                         <LinkButton
-                            href={from ?? route("admin.inventory.products")}
+                            href={from ?? route("products.index")}
                             className="mr-2 bg-red-500 text-white"
                         >
                             Back
@@ -267,7 +267,7 @@ export default function EditProduct({ product, categories, from }) {
                         </form>
                         <div className="mt-4">
                             <LinkButton
-                                href={from ?? route("admin.inventory.products")}
+                                href={from ?? route("products.index")}
                                 className="mr-2 bg-red-500 text-white"
                             >
                                 Back
