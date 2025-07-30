9c19670b74fbd2c4b29dc57366600b31e80306ac
Donz <donnygapulao@gmail.com>
2025-07-30 17:42:29 +0800

    featured products

--- app/Http/Controllers/FeaturedProductController.php
+++ app/Http/Controllers/FeaturedProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -12,6 +13,22 @@ class FeaturedProductController extends Controller
      */
     public function index()
     {
-        return Inertia::render('FeaturedProduct/Index');
+        $featuredProducts = Product::featuredProduct()->get();
+        return Inertia::render('FeaturedProduct/Index', compact('featuredProducts'));
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $request->validate([
+            'is_featured' => 'required|boolean',
+        ]);
+
+        $product->update([
+            'is_featured' => $request->input('is_featured'),
+        ]);
+
+        return response()->json([
+            'message' => 'Product updated successfully',
+        ]);
     }
 }
