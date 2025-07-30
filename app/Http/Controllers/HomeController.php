4b259b7778eace80b660eadfac9b0911d90fa4e2
Donz <donnygapulao@gmail.com>
2025-07-30 14:05:24 +0800

    updated title

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Traits\Traits\HasDefaultSeo;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class HomeController extends Controller
 {
+    use HasDefaultSeo;
+
     public function index(Request $request)
     {
-        $title = "Juanpasabuy Store";
+        $title = 'Home';
         $featuredProducts = Product::featuredProduct()->get()->random(4);
         // todo: replace with best products
         $bestProducts = Product::bestSeller()->get()->random(4);
