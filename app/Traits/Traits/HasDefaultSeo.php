9f026823140f60d5ca9d937252edf9f457a554e4
Donz <donnygapulao@gmail.com>
2025-07-30 14:02:56 +0800

    addd has default seo

---
+++ app/Traits/Traits/HasDefaultSeo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Traits\Traits;
+
+trait HasDefaultSeo
+{
+    public function __construct()
+    {
+        seo()->description("Juanpasabuy Store - Your one-stop online shopping destination in Dubai to the Philippines");
+    }
+}
