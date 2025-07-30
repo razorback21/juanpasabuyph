ffa329a0865f7d04023c423d85efee281b64db91
Donz <donnygapulao@gmail.com>
2025-07-30 14:15:50 +0800

    added image

--- app/Traits/Traits/HasDefaultSeo.php
+++ app/Traits/Traits/HasDefaultSeo.php
@@ -6,6 +6,8 @@ trait HasDefaultSeo
 {
     public function __construct()
     {
-        seo()->description("Juanpasabuy Store - Your one-stop online shopping destination in Dubai to the Philippines");
+        seo()
+            ->description("Juanpasabuy Store - Your one-stop online shopping destination in Dubai to the Philippines")
+            ->image(url('/storage/assets/share.png'));
     }
 }
