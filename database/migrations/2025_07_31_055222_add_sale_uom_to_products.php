3629937bd5d7d7d70aa29a1a942c2dd8def5f03c
Donz <donnygapulao@gmail.com>
2025-07-31 14:18:44 +0800

    added migration sale uom field to products

---
+++ database/migrations/2025_07_31_055222_add_sale_uom_to_products.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Enums\ProductUOMEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('sale_uom')->after('price')->default(ProductUOMEnum::PC->value);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('sale_uom');
+        });
+    }
+};
