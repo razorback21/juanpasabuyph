82ce31d11ceba9771aa7ba17248ae2a8e906b90e
Donz <donnygapulao@gmail.com>
2025-07-31 15:58:08 +0800

    chart area

--- resources/js/Pages/Dashboard/Index.jsx
+++ resources/js/Pages/Dashboard/Index.jsx
@@ -1,4 +1,5 @@
 //import { DataTable } from "@/components/data-table"
+import { ChartAreaInteractive } from "@/Components/chart-area-interactive";
 import { SectionCards } from "@/Components/section-cards";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
@@ -8,6 +9,9 @@ export default function ({ outOfStock }) {
         <AuthenticatedLayout header="Dashboard">
             <Head title="Dashboard" />
             <SectionCards outOfStock={outOfStock} />
+            <div className="grid grid-cols-1 mx-6">
+                <ChartAreaInteractive />
+            </div>
         </AuthenticatedLayout>
     );
 }
