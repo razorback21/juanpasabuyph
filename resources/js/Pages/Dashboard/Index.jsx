17cf8fe77492af9b3faa6b7bf6008c1384b429eb
Donz <donnygapulao@gmail.com>
2025-07-31 15:44:57 +0800

    of of stock

--- resources/js/Pages/Dashboard/Index.jsx
+++ resources/js/Pages/Dashboard/Index.jsx
@@ -1,11 +1,13 @@
 //import { DataTable } from "@/components/data-table"
+import { SectionCards } from "@/Components/section-cards";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
 
-export default function (props) {
+export default function ({ outOfStock }) {
     return (
         <AuthenticatedLayout header="Dashboard">
             <Head title="Dashboard" />
+            <SectionCards outOfStock={outOfStock} />
         </AuthenticatedLayout>
     );
 }
