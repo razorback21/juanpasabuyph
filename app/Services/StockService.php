2ea828317d252f3b992698e185fc3215705a060e
Donz <donnygapulao@gmail.com>
2025-07-31 15:57:50 +0800

    moved comment inside function

--- app/Services/StockService.php
+++ app/Services/StockService.php
@@ -14,15 +14,16 @@ class StockService
 
     }
 
-    //todo: refactor query. This not a performant query, best only few rows of products
+
     public function getOutOfStockProducts()
     {
+        //todo: refactor query. This not a performant query, best only few rows of products
         $result = Product::all()->where('available_stock', 0);
         $productCount = $result->count();
         if (!$productCount) {
             return collect([]);
         }
         $noStockIds = $result->pluck('id')->toArray();
-        return $outOfStockProducts = Product::whereIn('id', $noStockIds);
+        return Product::whereIn('id', $noStockIds);
     }
 }
