2bcf7feae5caf4ee72474e4bad71467081ba62ff
Donz <donnygapulao@gmail.com>
2025-08-01 18:08:20 +0800

    captcha config

---
+++ config/captcha.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'flat' => [
+        'length' => 5,
+        'width' => 200,
+        'height' => 40,
+        'quality' => 90,
+        'math' => false, //Enable Math Captcha
+        'expire' => 60 * 5, //Captcha expiration
+        'lines' => 5,
+    ]
+];
