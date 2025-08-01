69b581eaed6d1db9e861c335fb8bb956dac8a8fb
Donz <donnygapulao@gmail.com>
2025-08-01 18:07:29 +0800

    added store code

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContactFormRequest;
+use App\Notifications\ContactFormNotification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Notification;
 use Inertia\Inertia;
+use function Illuminate\Support\defer;
 
 class ContactController extends Controller
 {
     public function index()
     {
         return Inertia::render('Store/Contact/Index');
+    }
+
+    public function store(ContactFormRequest $request)
+    {
+        $validated = $request->validated();
+        defer(function () use ($validated) {
+            Notification::route('mail', config('app.admin_email'))->notify(new ContactFormNotification($validated));
+        });
+
+        return redirect()->back()->with('success', 'Your message has been sent.');
     }
 }
