30858bcdaf25e5d0ee78a2407bd7ebee05438fa7
Donz <donnygapulao@gmail.com>
2025-08-01 12:19:21 +0800

    refactor code

--- app/Services/SaleService.php
+++ app/Services/SaleService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Enums\OrderStatusEnum;
 use App\Models\Order;
+use Illuminate\Support\Facades\DB;
 
 class SaleService
 {
@@ -18,10 +19,10 @@ class SaleService
     public function getSaleProfitThisMonth(): float
     {
         $orders = Order::whereMonth('updated_at', now()->month)->where('status', OrderStatusEnum::SHIPPED->value)->get();
-        $profit = 0;
-        foreach ($orders as $order) {
-            $profit += $order->items->sum('net_profit');
-        }
-        return $profit;
+        $orderItems = DB::table('order_items')
+            ->select(DB::raw('SUM((price - cost_price) * quantity) as profit'))
+            ->whereIn('order_id', $orders->pluck('id')->toArray())
+            ->groupBy('order_id');
+        return (float) $orderItems->first()->profit;
     }
 }
