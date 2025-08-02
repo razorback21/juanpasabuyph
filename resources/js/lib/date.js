80f547065ca4c7cb02a7e6932c82fe12f8e1e667
Donz <donnygapulao@gmail.com>
2025-08-03 00:42:04 +0800

    updated date util

--- resources/js/lib/date.js
+++ resources/js/lib/date.js
@@ -1,9 +1,15 @@
 const dateFormatFriendly = (date) => {
-    return new Date(date).toLocaleString("en-US", {
+    const formattedDate = new Date(date).toLocaleString("en-US", {
         month: "long",
         day: "numeric",
         year: "numeric",
     });
+
+    if (formattedDate == "January 1, 1970") {
+        return null;
+    }
+
+    return formattedDate;
 };
 
 export { dateFormatFriendly };
