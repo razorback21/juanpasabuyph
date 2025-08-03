859d1c887499124a41fd73a31fa884b6b65dc2d2
Donz <donnygapulao@gmail.com>
2025-08-03 11:02:00 +0800

    seo files

---
+++ resources/views/vendor/seo/components/extensions/json.blade.php
@@ -0,0 +1,3 @@
+{{-- JSON extension component for SEO package --}}
+{{-- This component can be used to add JSON-LD structured data or other JSON-based meta information --}}
+{{-- Currently empty to resolve the missing component error --}}
---
+++ resources/views/vendor/seo/components/extensions/site.blade.php
@@ -0,0 +1,3 @@
+{{-- Site extension component for SEO package --}}
+{{-- This component can be used to add site-specific meta tags or other elements --}}
+{{-- Currently empty to resolve the missing component error --}}
