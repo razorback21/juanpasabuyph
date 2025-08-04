af234d8520558200e68b8268c8287252904cd674
Donz <donnygapulao@gmail.com>
2025-08-04 15:47:40 +0800

    remove console logs

--- resources/js/Pages/Store/components/Products.jsx
+++ resources/js/Pages/Store/components/Products.jsx
@@ -1,7 +1,6 @@
 import { Link } from "@inertiajs/react";
 
 export default function ({ products }) {
-    console.log(products);
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 p-4">
             {products.map((product) => (
