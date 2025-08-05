08f8f32f53f53ec99e4d93e7290ad0653b069c49
Donz <donnygapulao@gmail.com>
2025-08-05 18:49:30 +0800

    removed extra files

--- resources/views/vendor/seo/components/extensions/json.blade.php
+++
@@ -1,3 +0,0 @@
-{{-- JSON extension component for SEO package --}}
-{{-- This component can be used to add JSON-LD structured data or other JSON-based meta information --}}
-{{-- Currently empty to resolve the missing component error --}}
--- resources/views/vendor/seo/components/extensions/site.blade.php
+++
@@ -1,3 +0,0 @@
-{{-- Site extension component for SEO package --}}
-{{-- This component can be used to add site-specific meta tags or other elements --}}
-{{-- Currently empty to resolve the missing component error --}}
