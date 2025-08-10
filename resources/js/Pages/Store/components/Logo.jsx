c69a768b04f738a74de9b94106e6060703d24cfd
Donz <donnygapulao@gmail.com>
2025-08-10 10:29:26 +0800

    svg fixes

--- resources/js/Pages/Store/components/Logo.jsx
+++ resources/js/Pages/Store/components/Logo.jsx
@@ -10,9 +10,9 @@ export default function Logo() {
                         viewBox="0 0 24 24"
                         fill="none"
                         stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                     >
                         <path d="M11 21.73a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73z" />
                         <path d="M12 22V12" />
--- resources/js/Pages/Store/components/Reviews.jsx
+++ resources/js/Pages/Store/components/Reviews.jsx
@@ -37,10 +37,10 @@ export default function Reviews() {
                                     viewBox="0 0 24 24"
                                     fill="none"
                                     stroke="currentColor"
-                                    stroke-width="2"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    class="lucide lucide-user-round-icon lucide-user-round"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    className="lucide lucide-user-round-icon lucide-user-round"
                                 >
                                     <circle cx="12" cy="8" r="5" />
                                     <path d="M20 21a8 8 0 0 0-16 0" />
