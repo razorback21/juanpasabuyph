63996e4103e5943d2f7e48c9873b6c5babb07ac6
Donz <donnygapulao@gmail.com>
2025-08-21 10:57:20 +0000

    dev container initial setup

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        host: true, // This is equivalent to host: '0.0.0.0'
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            port: 5173,
+            host: 'localhost',
+        },
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
