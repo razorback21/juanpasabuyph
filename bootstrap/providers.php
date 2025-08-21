bb18f986eb057b64c7e69ecf199dba343437d97d
Donz <donnygapulao@gmail.com>
2025-08-21 16:05:04 +0000

    added laravel horizon

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\EventServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
     Buglinjo\LaravelWebp\WebpServiceProvider::class,
     Mews\Captcha\CaptchaServiceProvider::class,
