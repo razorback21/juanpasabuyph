f415c0b4db39f84213b0978be74d74c3bd333f1d
Donz <razorback@gmail.com>
2025-08-24 15:18:28 +0800

    optimized random products

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,7 +21,7 @@ class HomeController extends Controller
     public function index(Request $request)
     {
         $title = 'Home';
-        $featuredProducts = Product::featuredProduct()->inRandomOrder()->limit(4)->get();
+        $featuredProducts = Product::featuredProducts()->limit(4)->inRandomOrder()->get();
         $popularProducts = $this->productService->getPopularProducts()->inRandomOrder()->limit(4)->get();
         $heroImage = HeroImage::run('home');
         return Inertia::render("Store/Home/Index", compact('title', 'featuredProducts', 'popularProducts', 'heroImage'));
