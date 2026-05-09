25a0320446f6f451e19d2fb3126aa5f255b4ec3e
Your Namefree <razorback21@gmail.com>
2026-05-09 17:51:51 +0800

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,28 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    server: {
-        host: 'juanpasabuyph.loc',
-        port: 5173,
-        strictPort: true,
-        origin: 'http://juanpasabuyph.loc:5173',
-        cors: true,
-        hmr: {
-            port: 5173,
-            host: 'localhost',
-        },
+    // server: {
+    //     host: 'juanpasabuyph.loc',
+    //     port: 5173,
+    //     strictPort: true,
+    //     origin: 'http://juanpasabuyph.loc:5173',
+    //     cors: true,
+    //     hmr: {
+    //         port: 5173,
+    //         host: 'localhost',
+    //     },
 
+    // },
+    server: {
+        host: true,
+        port: 5173,
     },
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
-            ssr: 'resources/js/ssr.jsx',
+            input: "resources/js/app.jsx",
+            ssr: "resources/js/ssr.jsx",
             refresh: true,
         }),
         react(),
